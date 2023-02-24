bd022b06005541b82a8f33b2c9fb5b9977bf0e9a
leehy98 <117696170+leehy98@users.noreply.github.com>
2023-02-24 18:45:45 +0900

    Update Skill.cpp

--- TRPG/Skill.cpp
+++ TRPG/Skill.cpp
@@ -2,23 +2,26 @@
 #include <iostream>
 using namespace std;
 
-void warriorSkill(int* _bash)
+class Skill
 {
-	int _bashDamage = 50;
-	_bash = _bashDamage + (warriorAD / 3);
-}
-void wizardSKill(int* _magicMissle)
-{
-	int _magicMissleDamage = 30;
-	_magicMissle = _magicMissleDamage + (wizardAP / 7);
-}
-void pristSkill(int* _holyStrike)
-{
-	int _holyStrikeDamage = 20;
-	_holyStrike = _holyStrikeDamage + (pristAP / 3 + (pristAD / 5));
-}
-void vagabondSKill(int* _slash)
-{
-	int _slashDamage = 100;
-	_slash = _slashDamage + (vagabondAD/70);
+	void warriorSkill(int* _bash)
+	{
+		int _bashDamage = 50;
+		_bash = _bashDamage + (warriorAD / 3);
+	}
+	void wizardSKill(int* _magicMissle)
+	{
+		int _magicMissleDamage = 30;
+		_magicMissle = _magicMissleDamage + (wizardAP / 7);
+	}
+	void pristSkill(int* _holyStrike)
+	{
+		int _holyStrikeDamage = 20;
+		_holyStrike = _holyStrikeDamage + (pristAP / 3 + (pristAD / 5));
+	}
+	void vagabondSKill(int* _slash)
+	{
+		int _slashDamage = 100;
+		_slash = _slashDamage + (vagabondAD/70);
+	}
 }
